ca2cfa0fed4ea76674c4c5d02881e9e73479b523
Fatimakhafaji <151563981+Fatimakhafaji@users.noreply.github.com>
2025-03-30 22:57:52 +0300

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import CampaignPage from './components/CampaignPage';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <CampaignPage />
+    </div>
+  );
+}
+
+export default App;
